103d7a7b7c2e96e34bf92ab21dcf4166cf7c2a67
Saurabh Kumar <124427550+kumar02saurabh@users.noreply.github.com>
2024-10-07 23:17:14 +0530

    Add files via upload

---
+++ First.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+  
+  printf("Hello World");
+
+    return 0;
+}
+
+/*Sum Of Two Number*/
+
+#include<stdio.h>
+int main(){
+  int a,b;
+  printf("Enter the First No:");
+  scanf("%d",&a);
+
+  printf("Enter the Second No:");
+  scanf("%d",&b);
+
+  int sum=a+b;
+
+  printf("Sum of two number is: %d",sum);
+
+  return 0;
+
+}
