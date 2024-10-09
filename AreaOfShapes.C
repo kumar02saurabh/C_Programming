a108262c6b0d8da7c165b3d3921757a64058cd89
Saurabh Kumar <124427550+kumar02saurabh@users.noreply.github.com>
2024-10-09 10:43:31 +0530

    Add files via upload

---
+++ AreaOfShapes.C
@@ -0,0 +1,37 @@
+/*Write a Programm to find area of square*/
+// #include<stdio.h>
+// int main(){
+//     int side1,side2;
+//     printf("Enter the side of square:");
+//     scanf("%d",&side1);
+
+//     printf("Enter the side of square:");
+//     scanf("%d",&side2);
+
+//     int area = side1*side2;
+//     printf("Area of square is:%d",area);
+
+//     return 0;
+// }
+
+
+/*Write a Programm to find area of circle*/
+
+// #include<stdio.h>
+// int main(){
+//     float radius;
+//     printf("Enter the radius of circle");
+//     scanf("%f",&radius);
+
+//     float area=3.14*radius*radius;
+//     printf("Area of circle is:%f",area);
+
+//     return 0;
+// }
+
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("%d");
+    return 0;
+}
