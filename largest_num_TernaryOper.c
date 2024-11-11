f9137d8676680ccb52009994e1f6ebbe323d89cc
Saurabh Kumar <124427550+kumar02saurabh@users.noreply.github.com>
2024-11-11 11:43:10 +0530

    Create largest_num_TernaryOper.c

---
+++ largest_num_TernaryOper.c
@@ -0,0 +1,21 @@
+/*Documentation:
+Name: Saurabh Kumar 
+DOC: 11-11-2024
+Description: WAP to find largest of 3 number using Ternary Operator 
+Sample I/P: Enter three numbers : 5 10 15
+Sample O/P: Largest is 15
+*/
+#include<stdio.h>
+int main(){
+    
+    int a,b,max,c;
+    
+    printf("Enter two numbers:");
+    scanf("%d%d%d",&a,&b,&c);
+    
+    max=a>b?a:b>c?b:c;  // check condition which is largest 
+    printf("Lagerst is %d",max);
+    
+    
+    return 0;
+}
